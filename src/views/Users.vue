bb84ec425723ce268b17ebce57012136ace053f1
fernando zarco <fernandozarcoconde@gmail.com>
2025-06-06 20:02:58 -0400

    Filtro agregado

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="users">
+    <div class="search-bar">
+        <label for="">Busqueda por nombre</label>
+        <input type="text" v-model="query">
+        {{ query }}
+    </div>
     <table>
       <thead>
         <tr>
@@ -10,7 +15,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(user, index) of users" v-bind:key="index">
+        <tr v-for="(user, index) of filteredUsers" v-bind:key="index">
           <td>
             <button @click="$router.push(`/user/${user.id}`)">View</button>
             <!-- <button @click="viewUser(user.id)">View</button> -->
@@ -28,25 +33,29 @@
 
 <script setup>
 import axios from 'axios'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import User from './User.vue'
 import { useRouter } from 'vue-router'
 
 const users = ref([])
 const userId = ref(null)
 const router = useRouter()
+const query = ref('')
 
 onMounted(async () => {
   console.log('onmounted')
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
   users.value = data
-  console.log(users.value)
+  filteredUsers.value = [...users.value]
 })
 
-const viewUser = async (id) => {
-  console.log('viewUser', id)
-  userId.value = id
-}
+const viewUser = async (id) => userId.value = id
+
+const filteredUsers = computed(() =>
+  users.value.filter(user =>
+    user.name.toLowerCase().includes(query.value.toLowerCase())
+  )
+)
 </script>
 <style scoped>
 .users {
